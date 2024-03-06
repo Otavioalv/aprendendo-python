92440728e597dcf26f20460d399d330f38e656d3
Otavioalv <otaviogabriel.alves.barbosa@gmail.com>
2024-03-06 11:02:27 -0400

    Commit

---
+++ classe.py
@@ -0,0 +1,11 @@
+class Test:
+        def func1():
+            print("função 1 : aaasdfdss ");
+        
+        def func2(variavel:str):
+            print("função 2 >> ", variavel);
+        
+        def func3(var1: str, var2 = "opcional"):
+            print("Função 3 >>> ");
+            print("\n var1 Obrigatoria : ", var1);
+            print("\n var2 Opcional : ", var2);
---
+++ explicação.py
@@ -0,0 +1,8 @@
+from classe import Test;
+
+test = Test;
+parametro = "parametro em variavel"
+
+test.func1();
+test.func2(parametro);
+test.func3(parametro, "ouutro valor");
